1ea10767df022a7b83648980362157eee244c5ab
Nitesh Kumar <niteshman30@gmail.com>
2023-03-24 18:32:18 +0530

    firstCommit

--- app/src/androidTest/java/com/cheezycode/quotesviewmodel/MainActivityTest.kt
+++ app/src/androidTest/java/com/cheezycode/quotesviewmodel/MainActivityTest.kt
@@ -18,26 +18,22 @@ import org.junit.Test
 class MainActivityTest{
 
     @get:Rule
-    val activityScenarioRule = activityScenarioRule<MainActivity>()
-
+    val activityScenarioRule= activityScenarioRule<MainActivity>()
     @Test
-    fun testNextButton_expectedCorrectQuote(){
-
-        onView(withId(R.id.btnNext)).perform(click())
-        onView(withId(R.id.btnNext)).perform(click())
-        onView(withId(R.id.btnNext)).perform(click())
-        onView(withId(R.id.quoteText)).check(matches(withText("Difficulties increase the nearer we get to the goal.")))
-    }
-
+   fun test_nextButton_click_changeQuote(){
+       onView(withId(R.id.btnNext)).perform(click())
+       onView(withId(R.id.btnNext)).perform(click())
+       onView(withId(R.id.btnNext)).perform(click())
+       onView(withId(R.id.quoteText)).check(matches(withText("Difficulties increase the nearer we get to the goal.")))
+   }
     @Test
-    fun testShareButton_expectedIntentChooser(){
-        Intents.init()
-        val expected = allOf(hasAction(Intent.ACTION_SEND))
+    fun testShareButton(){
+       Intents.init()
+        val expected= allOf(hasAction(Intent.ACTION_SEND))
         onView(withId(R.id.floatingActionButton)).perform(click())
         intended(expected)
         Intents.release()
     }
-
 }
 
 
